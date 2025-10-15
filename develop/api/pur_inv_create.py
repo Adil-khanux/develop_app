57b9e7db68fb8bfed5be67f4e6751d17f495c053
Adil-khanux <8218adilkhan@gmail.com>
2025-10-15 17:17:03 +0530

    Update purchase invoice case

--- develop/api/pur_inv_create.py
+++ develop/api/pur_inv_create.py
@@ -1,50 +1,62 @@
 import frappe
 
 def create_purchase_invoice(doc, method):
-    for item in doc.items:  # Use doc.items, not items
-        # Get item type
+    for item in doc.items:
         item_type = frappe.db.get_value("Item", item.item_code, "custom_item_type")
-        # frappe.msgprint(f"item type is { item_type }")
-        
         if item_type != "Consignment":
             frappe.msgprint(f"Item {item.item_code} is not Consignment")
-            continue  # skip non-consignment items
-        
-        # Get oldest Purchase Receipt for this item
-        older_receipt = frappe.db.sql("""
-            SELECT 
-                pri.parent AS purchase_receipt,  
-                pri.rate,
-                pri.item_code,
-                pr.name,
-                pr.supplier         
-            FROM `tabPurchase Receipt Item` AS pri
-            JOIN `tabPurchase Receipt` AS pr
-                ON pr.name = pri.parent
-            WHERE pri.item_code = %s
-            ORDER BY pr.posting_date ASC
-            LIMIT 1
-        """, (item.item_code,), as_dict=True)
-        
-        if not older_receipt:
-            # frappe.msgprint(f"No Purchase Receipt found for item {item.item_code}")
             continue
-        pi = frappe.new_doc("Purchase Invoice")
-        #  pick  first receipt from dictionary 
-        receipt = older_receipt[0]
-        
-        # Create new Purchase Invoice
-        pi.supplier = receipt["supplier"]  # supplier from parent Purchase Receipt
-        
-        # Append item
-        pi.append("items", {
-            "item_code": receipt["item_code"],
-            "rate": receipt["rate"],
-            "qty": item.qty  ,
-            "purchase_receipt" : receipt["name"],
-           
-        })
-        # Save and submit
-        pi.insert()
-        pi.submit()
-        frappe.msgprint(f"Purchase Invoice {pi.name} created for item {item.item_code}")
+
+        remaining_qty = item.qty
+        created_invoices = {}
+
+        receipts = frappe.db.sql("""
+                    SELECT 
+                        pri.parent AS purchase_receipt,  
+                        pri.rate,
+                        pri.item_code,
+                        pri.name as row_name,
+                        pri.received_qty,
+                        (pri.qty - (pri.qty * IFNULL(pri.billed_amt, 0) / IFNULL(pri.net_amount, 1))) AS remaining_qty,
+                        pr.name,
+                        pr.supplier         
+                    FROM `tabPurchase Receipt Item` AS pri
+                    JOIN `tabPurchase Receipt` AS pr
+                        ON pr.name = pri.parent
+                    WHERE pri.item_code = %s 
+                        AND pr.docstatus = 1
+                        AND (pri.qty - (pri.qty * IFNULL(pri.billed_amt, 0) / IFNULL(pri.net_amount, 1))) > 0
+                    ORDER BY pr.posting_date ASC
+                """, (item.item_code,), as_dict=True)
+
+
+        for receipt in receipts:
+            if remaining_qty <= 0:
+                break
+
+            grn = receipt["name"]
+            supplier = receipt["supplier"]
+            available_qty = receipt["remaining_qty"]
+
+            if supplier not in created_invoices:
+                pi = frappe.new_doc("Purchase Invoice")
+                pi.supplier = supplier
+                created_invoices[supplier] = pi
+            else:
+                pi = created_invoices[supplier]
+
+            qty_to_bill = min(remaining_qty, available_qty)
+            remaining_qty -= qty_to_bill
+
+            pi.append("items", {
+                "item_code": receipt["item_code"],
+                "rate": receipt["rate"],
+                "qty": qty_to_bill,
+                "purchase_receipt": grn,
+                "pr_detail": receipt["row_name"]
+            })
+
+        for supplier, pi in created_invoices.items():
+            pi.insert()
+            pi.submit()
+            frappe.msgprint(f"Purchase Invoice {pi.name} created for supplier {supplier}")
